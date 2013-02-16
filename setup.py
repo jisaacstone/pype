01c7e15822baa72171f969cf67a35d80bc34e2de
jisaacstone <jisaacstone.gmail.com>
2013-02-15 16:44:13 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pype',
+      version='0.1',
+      description='Tiny helper for command-line python',
+      author='jisaacstone',
+      author_email='jisaacstone+python@gmail.com',
+      url='https://github.com/jisaacstone/pype',
+      scripts=['pype'],
+     )

967bef20d4f50e156435ad721f025eca6136c0fb
jistone <jistone@ebay.com>
2013-02-15 23:03:10 -0800

    getting ready for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
 
 setup(name='pype',
       version='0.1',
       description='Tiny helper for command-line python',
+      long_description=open(os.path.join(os.path.dirname(__file__),
+                                         'readme.md')).read(),
       author='jisaacstone',
       author_email='jisaacstone+python@gmail.com',
+      license='MIT',
       url='https://github.com/jisaacstone/pype',
       scripts=['pype'],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Topic :: Utilities",
+                   "License :: OSI Approved :: MIT License"],
      )
